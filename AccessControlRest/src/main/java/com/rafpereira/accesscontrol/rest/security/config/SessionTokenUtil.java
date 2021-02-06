3893d553ae9bf81c70ae4bd62335d11344a8a9db
rafaeldearaujopereira <rafael.a.pereira@gmail.com>
2021-02-06 10:50:34 -0300

    adding javadoc

--- AccessControlRest/src/main/java/com/rafpereira/accesscontrol/rest/security/config/SessionTokenUtil.java
+++ AccessControlRest/src/main/java/com/rafpereira/accesscontrol/rest/security/config/SessionTokenUtil.java
@@ -11,10 +11,20 @@ import com.rafpereira.accesscontrol.business.util.SessionUtil;
 import com.rafpereira.accesscontrol.model.Session;
 import com.rafpereira.accesscontrol.model.util.LogExtraInfo;
 
+/**
+ * Util class to provide a simple way to perform procedures related to session and token.
+ * @author rafaeldearaujopereira
+ *
+ */
 public class SessionTokenUtil {
 
 	private static final String BEARER = "Bearer";
 
+	/**
+	 * Obtains the active session (when active) using a given token.
+	 * @param request Request
+	 * @return Session (Access Control)
+	 */
 	public static Session getSessionByToken(HttpServletRequest request) {
 		String param = request.getHeader(HttpHeaders.AUTHORIZATION);
 		if (param == null) {
@@ -36,6 +46,12 @@ public class SessionTokenUtil {
 		return session;
 	}
 	
+	/**
+	 * Creates the audit bean, used to transport the information.
+	 * @param request Request
+	 * @param token Token
+	 * @return Bean that transports data to persist audit.
+	 */
 	public static LogExtraInfo createTrackingLogInfo(HttpServletRequest request, String token) {
 		String ipAddress = request.getRemoteAddr();
 		String hostName = request.getRemoteHost().toUpperCase();
